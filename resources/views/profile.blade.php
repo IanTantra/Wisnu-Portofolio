29cf16b7bd9b7d51039e40e242f25afd393b4f41
IanTantra <mycollegeian@gmail.com>
2025-01-13 22:01:21 +0700

    commit for profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 
 <div class="clearfix">
-    <img src="/img/{{ $image }}" class="col-md-4 float-md-end mb-3 ms-md-3 mt-3 img-thumbnail img-fluid rounded height-2000" alt="profile" max-width="50%" height="50%">
+    <img src="{{ asset('img/' . $image) }}" class="col-md-4 float-md-end mb-3 ms-md-3 mt-3 img-thumbnail img-fluid rounded height-2000" alt="profile" max-width="50%" height="50%">
   
     <h1 class="text mt-3 mb-3">{{ $name }}</h1>
   
