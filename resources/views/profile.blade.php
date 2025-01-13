ab824bf3dbba4055690c53e494e92732528136a9
IanTantra <mycollegeian@gmail.com>
2025-01-13 22:07:08 +0700

    test profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 
 <div class="clearfix">
-    <img src="{{ asset('img/' . $image) }}" class="col-md-4 float-md-end mb-3 ms-md-3 mt-3 img-thumbnail img-fluid rounded height-2000" alt="profile" max-width="50%" height="50%">
+    <img src="https://id.m.wikipedia.org/wiki/Berkas:Naruto_logo.svg" class="col-md-4 float-md-end mb-3 ms-md-3 mt-3 img-thumbnail img-fluid rounded height-2000" alt="profile" max-width="50%" height="50%">
   
     <h1 class="text mt-3 mb-3">{{ $name }}</h1>
   
